32906b51f6f52ac4ea43f7b77a69f91c3f395e57
Sumit Pal <asr.sumitpal92@gmail.com>
2021-12-11 03:59:46 -0500

    Removed Unnacessary namespaces

--- HotelManagementWEB/Default.aspx.cs
+++ HotelManagementWEB/Default.aspx.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Configuration;
 using System.Data;
-using System.Web;
-using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Data.SqlClient;  
+using System.Data.SqlClient;
 
 public partial class _Default : System.Web.UI.Page
 {
